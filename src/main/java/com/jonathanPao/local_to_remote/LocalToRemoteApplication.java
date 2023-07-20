426f944bd62c4768569f9a530bb816d03c685146
Jonathan <jonathan66652@gmail.com>
2023-07-20 17:00:56 +0800

    程式初版

---
+++ src/main/java/com/jonathanPao/local_to_remote/LocalToRemoteApplication.java
@@ -0,0 +1,13 @@
+package com.jonathanPao.local_to_remote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalToRemoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalToRemoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathanPao/local_to_remote/LocalToRemoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathanPao.local_to_remote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalToRemoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
