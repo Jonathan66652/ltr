0836c23fb4b29279d1b22c0669c0660d7fdee43f
Jonathan <jonathan66652@gmail.com>
2023-07-21 10:05:38 +0800

    新增test api for 合併衝突

--- src/main/java/com/jonathanPao/local_to_remote/app/TestController.java
+++ src/main/java/com/jonathanPao/local_to_remote/app/TestController.java
@@ -1,8 +1,13 @@
 package com.jonathanPao.local_to_remote.app;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("api")
 public class TestController {
-
+	
+	@GetMapping("/test")
+	public String getInfo() {
+		return "";
+	}
 }
